7fecf9a68bbb31fefa8299190d455047a9879d01
tronicbaumi <christoph.baumgartner@gmx.at>
2024-06-07 06:30:39 +0200

    buttons working

--- fw/bottle_tap/bottle_tap.ino
+++ fw/bottle_tap/bottle_tap.ino
@@ -17,58 +17,134 @@
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MOTOR_STEPS 200
 // Target RPM for X axis motor
-#define MOTOR_X_RPM 100
+#define MOTOR_BOTTLE_RPM 100
 // Target RPM for Y axis motor
-#define MOTOR_Y_RPM 100
+#define MOTOR_GLASS_RPM 100
+
+#define ENABLE_BOTTLE 12
+#define ENABLE_GLASS  9
 
 // X motor
-#define DIR_X 10
-#define STEP_X 11
+#define DIR_BOTTLE 10
+#define STEP_BOTTLE 11
 
 // Y motor
-#define DIR_Y 7
-#define STEP_Y 8
+#define DIR_GLASS 7
+#define STEP_GLASS 8
 
 // If microstepping is set externally, make sure this matches the selected mode
 // 1=full step, 2=half step etc.
 #define MICROSTEPS 2
 
+#define BOTTLE_UP 6
+#define BOTTLE_DOWN 5
+#define GLASS_UP 3
+#define GLASS_DOWN 4
+
+int bottle_up;
+int bottle_down;
+int bottle_enable;
+int glass_up;
+int glass_down;
+int glass_enable;
+
 // 2-wire basic config, microstepping is hardwired on the driver
 // Other drivers can be mixed and matched but must be configured individually
-BasicStepperDriver stepperX(MOTOR_STEPS, DIR_X, STEP_X);
-BasicStepperDriver stepperY(MOTOR_STEPS, DIR_Y, STEP_Y);
+BasicStepperDriver stepperBOTTLE(MOTOR_STEPS, DIR_BOTTLE, STEP_BOTTLE);
+BasicStepperDriver stepperGLASS(MOTOR_STEPS, DIR_GLASS, STEP_GLASS);
 
 // Pick one of the two controllers below
 // each motor moves independently, trajectory is a hockey stick
 // MultiDriver controller(stepperX, stepperY);
 // OR
 // synchronized move, trajectory is a straight line
-SyncDriver controller(stepperX, stepperY);
+SyncDriver controller(stepperBOTTLE, stepperGLASS);
 
 void setup() {
-    /*
-     * Set target motors RPM.
-     */
-    stepperX.begin(MOTOR_X_RPM, MICROSTEPS);
-    stepperY.begin(MOTOR_Y_RPM, MICROSTEPS);
-    // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next two lines
-    // stepperX.setEnableActiveState(LOW);
-    // stepperY.setEnableActiveState(LOW);
+  Serial.begin(115200);
+  Serial.println("bottle tap started....");
+  Serial.println("==========================");
+  /*
+    * Set target motors RPM.
+    */
+  Serial.print("init motors ... ");
+  stepperBOTTLE.begin(MOTOR_BOTTLE_RPM, MICROSTEPS);
+  stepperGLASS.begin(MOTOR_GLASS_RPM, MICROSTEPS);
+  Serial.println("done !");
+  // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next two lines
+  // stepperX.setEnableActiveState(LOW);
+  // stepperY.setEnableActiveState(LOW);
+
+  pinMode(BOTTLE_UP, INPUT);
+  pinMode(BOTTLE_DOWN, INPUT);
+
+  pinMode(GLASS_UP, INPUT);
+  pinMode(GLASS_DOWN, INPUT);
+
+  pinMode(ENABLE_BOTTLE, OUTPUT);
+  pinMode(ENABLE_GLASS, OUTPUT);
+
+  digitalWrite(ENABLE_BOTTLE,HIGH);
+
 }
 
 void loop() {
+  //Serial.println("loop !");
 
-    controller.rotate(180, 0);
-    delay(1000);
-    controller.rotate(-180, 0);
-    // delay(1000);
-    // controller.rotate(0, -30*15);
-    delay(2000);
+  bottle_up = digitalRead(BOTTLE_UP);
+  bottle_down = digitalRead(BOTTLE_DOWN);
+  glass_up = digitalRead(GLASS_UP);
+  glass_down = digitalRead(GLASS_DOWN);
+  
 
-    controller.rotate(0, 180);
-    delay(1000);
-    controller.rotate(0,-180);
-    // delay(1000);
-    // controller.rotate(0, -30*15);
-    delay(2000);
+  if (bottle_up == LOW) {
+    
+    Serial.print("BOTTLE UP ");
+    controller.rotate(1, 0);
+    
+  } else {
+    
+    
+  }
+
+  if (bottle_down == LOW) {
+    
+    Serial.print("BOTTLE DOWN ");
+    controller.rotate(-1, 0);
+    
+  } else {
+    
+    
+  }
+
+    if (glass_up == LOW) {
+    Serial.print("GLASS UP ");
+    
+  } else {
+    
+    
+  }
+
+  if (glass_down == LOW) {
+    Serial.print("GLASS DOWN ");
+    
+  } else {
+    
+    
+  }
+  Serial.println();
+
+  // controller.rotate(180, 0);
+  // delay(1000);
+  // controller.rotate(-180, 0);
+  // // delay(1000);
+  // // controller.rotate(0, -30*15);
+  // delay(2000);
+
+  // controller.rotate(0, 180);
+  // delay(1000);
+  // controller.rotate(0,-180);
+  // // delay(1000);
+  // // controller.rotate(0, -30*15);
+  // delay(2000);
 }
